f90b3e00f61078b46b8fa014832e1cd70f1087f6
Teodoro <t177398@dac.unicamp.br>
2019-03-22 16:24:10 -0300

    Token alterado para bot TeleWeather

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from vai_chover_bot import WeatherBot
 
+# TOKEN DO MARMITESTE
+# telegram_token = '822196045:AAFc0D070aSIBmlWW3-PZT9efvcrHMi_1hk'
 
-telegram_token = '822196045:AAFc0D070aSIBmlWW3-PZT9efvcrHMi_1hk'
+# TOKEN DO TeleWeather
+telegram_token = '609279282:AAGSejkH6fvq4iVY2wjohK0dLl-3MN5Ry-0'
+
+
 open_weather_token = '2a6a25c487d5b3c8b34d9bc0ea1909f3'
 
 bot = WeatherBot(telegram_token, open_weather_token)
